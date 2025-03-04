f46d08bfff132f75cc4fb1215ffeaf2e84e68bb5
RichardPitoniak <richard106285@student-schoolsofwestfield.org>
2025-03-04 15:55:07 +0000

    done (also I made the button work)

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 /* Var, Prompt, Confirm, Alert, Compound Conditional */
 
 // Create pet variable, set equal to empty quotes ("")
+function button () {
 var pet = ("")
 // Ask if they are allergic, store in a boolean variable with OK/Cancel
 var allergic = confirm ("are you allergic to pet hair? Ok for yes, cancel for no")
 // If they are allergic, ask if they want a cute pet, store in a boolean variable with OK/Cancel
-if (allergic == true) var cute = prompt ("do you want a cute pet? Ok for yes, cancel for no")
+if (allergic == true) var cute = confirm ("do you want a cute pet? Ok for yes, cancel for no")
 // If they are not allergic, ask if they want to be outdoors with their pet.  Store it in a boolean with OK/Cancel.
 else var outdoors = confirm ("do you want to be outside with your pet? Ok for yes, cancel for no")
 // If allergic and cute, pet =  bird
@@ -24,4 +25,4 @@ alert ("you would do well with a " + pet)
 var petname = prompt ("name your pet")
 // Display petname the pet says hi. 
 alert (petname + " the " + pet + " says hi")
-
+};
